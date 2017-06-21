d3816741d1e562b1932d825513d497d04237b152
Chelsea Hartman <c.overstreet@vibethink.com>
2017-06-21 15:15:01 -0400

    Default Template

--- wp-content/themes/tuckandsons/page.php
+++ wp-content/themes/tuckandsons/page.php
@@ -2,19 +2,24 @@
 	
 	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
-    	<?php if(get_field('page_builder')): ?>
-		
-			<?php get_template_part('page-builder/loop','page_builder'); ?>
-		
-		<?php else: ?>
+		<div id="content">
+	
+		<div id="inner-content" class="row">
 
-			<div class="row">
-				<div class="columns small-12">
-					<?php the_content(); ?>
+			<div class="top-header" style="background-image: url('<?php the_field('hero_image'); ?>'); background-repeat: no-repeat; background-size: cover; background-position: center;">
+				<h1><?php the_title(); ?></h1>
+			</div>
+			<div class="main-content-wrap">
+				<div class="main-content">
+					<?php the_field('content'); ?>
 				</div>
 			</div>
+		    
+		</div> <!-- end #inner-content -->
+	
+	</div> <!-- end #content -->
 
-		<?php endif; ?>
+		
     
     <?php endwhile; endif; ?>							
 
